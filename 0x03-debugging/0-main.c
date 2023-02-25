43cacc53316a95a91b912d29018e8f39c90eea0f
myUsername <myemail@gmail.com>
2023-02-24 20:22:59 -0800

    Testing my code for bugs

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests that prints if integer is positive or negative
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_negative(i);
+
+	return (0);
+}
