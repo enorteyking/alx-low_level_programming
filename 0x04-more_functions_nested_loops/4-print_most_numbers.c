b585bcc84b759af0f79e1edef9d9342b00eebd05
myUsername <myemail@gmail.com>
2023-02-27 04:58:35 -0800

    A function that prints the numbers, from 0 to 9 except 2 & 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * print_most_numbers - 'function that prints the numbers, from 0 to 9 except 2 & 4'
+ * print_most_numbers - 'function that prints the numbers, 
+ * from 0 to 9 except 2 & 4'
  */
 
 void print_most_numbers(void)
