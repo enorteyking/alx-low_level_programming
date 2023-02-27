745f93fb612657f318f0842cd5f54da1a98b5600
myUsername <myemail@gmail.com>
2023-02-27 05:47:33 -0800

    FizzBuzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 100; i++)
 		if (i % 15 == 0)
 		{
 			printf("FizzBuzz ");
@@ -26,7 +26,7 @@ int main(void)
 		{
 			printf("%d ", i);
 		}
-/*	printf("Buzz");*/
+	printf("Buzz");
 	printf("\n");
 	return (0);
 }
