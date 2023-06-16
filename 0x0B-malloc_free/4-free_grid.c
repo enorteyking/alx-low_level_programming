4c02676383c1778c08e6e6ea663211742aeb2a2e
Your Name <you@example.com>
2023-06-16 01:41:54 -0700

    Function free up grid allocated by another function, alloc_grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - frees up grid allocated by alloc_grid
+ * @grid: grid to free
+ * @height: heightof grid
+ *
+ * Return: NULL if it fail
+ */
+void free_grid(int **grid, int height)
+{
+	if (!grid)
+		return;
+	if (!grid[0])
+		return;
+	while (height-- > 0)
+		free(grif[height]);
+	free(grid);
+}
